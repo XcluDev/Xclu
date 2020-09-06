f8eada00c740ebb74dfa5e0a39916ed6e8f04d92
perevalovds <perevalovds@gmail.com>
2020-09-06 11:14:56 +0500

    Making "Links" group

--- core/xclu_theme.h
+++ core/xclu_theme.h
@@ -48,7 +48,7 @@ const int LABEL_WIDTH_MIN = 80;  //ширина label
 const int LINE_EDIT_WIDTH = 200;  //ширина строки
 const int COMBO_WIDTH_MIN = 150;  //ширина enum
 const int COMBO_WIDTH_MAX = 300;
-const int BUTTON_WIDTH = 120;      //ширина button
+const int BUTTON_WIDTH = 200;      //ширина button
 
 const int SPIN_WIDTH = 120;      //ширина int, float
 
